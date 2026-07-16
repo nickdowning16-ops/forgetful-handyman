add90e90a1cf91c511fea0e497c5628fe50738cd
anthonyrvance <48871259+anthonyrvance@users.noreply.github.com>
2026-07-15 20:52:05 -0400

    kept hbox moved control to child for movement

--- Scripts/UI/Thought Loops/loop_mod.gd
+++ Scripts/UI/Thought Loops/loop_mod.gd
@@ -11,24 +11,24 @@ func _process(delta):
 	pass
 
 func _on_area_2d_mouse_entered():
-	$Panel/LoopDescription.visible = true
-	self.scale = Vector2(1.2, 1.2)
+	$EditableContainer/LoopDescription.visible = true
+	$EditableContainer.scale = Vector2(1.2, 1.2)
 
 func _on_area_2d_mouse_exited():
-	$Panel/LoopDescription.visible = false
-	self.scale = Vector2(1.0, 1.0)
+	$EditableContainer/LoopDescription.visible = false
+	$EditableContainer.scale = Vector2(1.0, 1.0)
 
 func spawn():
 	# play anim of moving
-	self.modulate.a = 0.0
-	self.position.y += rise_distance
+	$EditableContainer.modulate.a = 0.0
+	$EditableContainer.position.y += rise_distance
 
 	var tween = create_tween()
 	tween.set_parallel(true)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_trans(Tween.TRANS_SINE)
 
-	tween.tween_property(self, "modulate:a", 1.0, fade_duration)
-	tween.tween_property(self, "position:y", self.position.y - rise_distance, fade_duration)
+	tween.tween_property($EditableContainer, "modulate:a", 1.0, fade_duration)
+	tween.tween_property($EditableContainer, "position:y", $EditableContainer.position.y - rise_distance, fade_duration)
 	
 	# play anim of some idle like glow
